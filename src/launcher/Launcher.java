19febdd6d7a902b92aef6c5d187f3bf97e044763
Sankey5 <briansankey93@gmail.com>
2019-06-30 20:41:46 -0500

    added a launcher and connected it to the login page

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,18 +1,36 @@
 package launcher;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Launcher {
+public class Launcher extends Application{
 	
-	public void Start(Stage stage) extends Application{
+	@Override
+	public void start(Stage stage){
+		try {
+			FXMLLoader loader = new FXMLLoader(Launcher.class.getResource("/view/login.fxml"));
+			
+			Parent root = loader.load();
+			
+			Scene scene = new Scene(root);
+			
+			stage.setTitle("Job Queue");
+			stage.setScene(scene);
+			stage.show();
+		}catch(IOException e) {
+			System.out.println("Could not find FXML file");
+			return;
+		}
 		
 	}
 	
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
