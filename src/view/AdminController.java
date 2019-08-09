14b664bd2dd08c8beaa564418d50d2e8e2368538
KennethWebber <31325329+KennethWebber@users.noreply.github.com>
2019-08-09 15:02:27 -0500

    Restored lost code

--- src/view/AdminController.java
+++ src/view/AdminController.java
@@ -57,7 +57,6 @@ public class AdminController implements EventHandler<ActionEvent>, Initializable
 			popupWindow.setScene(new Scene(root));
 			popupWindow.show();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -72,7 +71,14 @@ public class AdminController implements EventHandler<ActionEvent>, Initializable
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+<<<<<<< HEAD
 		// TODO Auto-generated method stub
 		
+=======
+		ObservableList<Customer> clients = FXCollections.observableArrayList(database.getCustomers());
+		clientListView.itemsProperty().set(clients);
+		inventoryListView.itemsProperty().set(FXCollections.observableArrayList(database.getEquipment()));
+		jobsListView.itemsProperty().set(FXCollections.observableArrayList(database.getJobs()));
+>>>>>>> stash
 	}
 }
