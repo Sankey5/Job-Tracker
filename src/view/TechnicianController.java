a81d3805d806fd23dd2b6b4d282bd75846841264
AlloyRod <31325193+AlloyRod@users.noreply.github.com>
2019-08-09 23:56:49 -0500

    Edit details of Job

--- src/view/TechnicianController.java
+++ src/view/TechnicianController.java
@@ -36,6 +36,8 @@ public class TechnicianController implements EventHandler<ActionEvent>, Initiali
 	private DatePicker startDatePicker, endDatePicker;
 	@FXML
 	private TextArea detailsTextArea, extrasTextArea;
+	@FXML
+	private TextField editField;
 	
 	private Technician tech;
 	private Database database;
@@ -150,6 +152,20 @@ public class TechnicianController implements EventHandler<ActionEvent>, Initiali
 		currentJobsListView.setItems(FXCollections.observableArrayList(tech.getMyJobs()));
 		availableJobsListView.setItems(FXCollections.observableArrayList(Database.getInstance().getJobs()));
 		extrasTextArea.setText(tech.toExtras());
+		}
+	}
+	
+	public void currentMenuDetails(ActionEvent event) {
+		if(currentJobsListView.getSelectionModel().isEmpty()){
+			return;
+		} else {
+			Job current = currentJobsListView.getSelectionModel().getSelectedItem();
+			if(editField.getText().isEmpty()) {
+				return;
+			}
+			current.setMemo(editField.getText());
+			detailsTextArea.setText(current.toDescription());
+			editField.clear();
 		}
 	}
 	
