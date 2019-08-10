cddf2110adc0cd24a85e47d0b713c17d40667b26
AlloyRod <31325193+AlloyRod@users.noreply.github.com>
2019-08-10 01:03:55 -0500

    dateSorter

---
+++ src/model/DateSorter.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.Comparator;
+
+public class DateSorter implements Comparator<Job>{
+
+	@Override
+	public int compare(Job j1, Job j2) {
+		// TODO Auto-generated method stub
+		return j2.getDeadlineEntry().compareTo(j1.getDeadlineEntry());
+	}
+	
+
+}
